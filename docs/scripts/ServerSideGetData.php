2083c2b2080b6525fabb4ad0ee67faaa22d93ee5
joeh28 <76986336+joeh28@users.noreply.github.com>
2021-02-22 12:33:48 -0500

    Update ServerSideGetData.php

--- docs/scripts/ServerSideGetData.php
+++ docs/scripts/ServerSideGetData.php
@@ -1,13 +1,13 @@
 <?php
         //We thank DontMissTheFlight for the making of this script
-include( "../includes/db_connect.php" );
+	include( "../includes/db_connect.php" );
 
-//variables from the user/program/game
+	//variables from the user/program/game
 
 
-//Connect to database
+	//Connect to database
 
-$conn = sqlsrv_connect( $serverName, $connectionInfo);
+	$conn = sqlsrv_connect( $serverName, $connectionInfo);
 		
 		//Check connection
 		if ($conn->connect_error)
@@ -15,5 +15,26 @@ $conn = sqlsrv_connect( $serverName, $connectionInfo);
 			die("Connection failed: " . $conn->connect_error);
 		}
 		echo "Connected Successfully";
+		
+		
+	//SQL Command to get player info
+	$sql = "SELECT TOP 5 playername, highscore FROM OhareSays ORDER BY highscore DESC";
+	$result = $conn->query($sql);
+	
+	if ($result->num_rows > 0)
+	{
+		//output data of each row
+		while($row = $result->fetch_assoc())
+		{
+			echo $row["playername"]. " ". $row["highscore"]. "<br>";
+		}
+		else
+		{
+			echo "0 results";
+		}
+		
+	}
+	$conn->close();
+
 		
 ?>
