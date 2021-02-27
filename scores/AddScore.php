17839c7e20025c0c2d3fa08e114af69cbd91ccdb
joeh28 <76986336+joeh28@users.noreply.github.com>
2021-02-26 20:36:02 -0500

    Update AddScore.php

--- scores/AddScore.php
+++ scores/AddScore.php
@@ -2,7 +2,7 @@
 
 	$debug = false;
 
-        include 'mysql_connect.php';
+        include( "../includes/db_connect.php" );
         //These are our variables.
         //We use real escape string to stop people from injecting. We handle this in Unity too, but it's important we do it here as well in case people extract our url.
         $redirect_url = "https://levidsmith.com/scores/DisplayScores.html";
